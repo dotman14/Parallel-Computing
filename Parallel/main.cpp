cfdcfb4a682474f1a1363ce2d09eba61e01dbf27
dotman14 <dotunoyesanmi@gmail.com>
2016-10-01 15:02:52 -0400

    Initial Commit

---
+++ Parallel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parallel
+//
+//  Created by Oyedotun Oyesanmi on 10/1/16.
+//  Copyright © 2016 Oyedotun Oyesanmi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
